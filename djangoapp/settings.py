e52392231d7bf4d3328a40688b866a0370170149
saumitra619 <saumitra619@gmail.com>
2023-04-29 22:05:31 +0530

    new changes

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djangoapp.urls'
@@ -118,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'blog/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'productionfiles')
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'assets')
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
